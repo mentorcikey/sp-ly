cbecf0109cd996e43b5d3f9a42de5c7ff12e612c
mentorcikey <citeducation2604@mgmail.com>
2023-09-29 12:47:35 +0700

    abc

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Login from './components/Login';
+import User from './components/User';
+// import Register from "./components/Register.jsx"
+
+function App() {
+  return (
+    <div className="app">
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <User />
+    </div>
+  );
+}
+
+export default App;
